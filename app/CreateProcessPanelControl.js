fd01b5227bc1de962f749a6d047657d1d36c530e
Demitri Grant <demitri.grant@gmail.com>
2021-11-25 22:38:52 -0500

    Update 5

--- app/CreateProcessPanelControl.js
+++ app/CreateProcessPanelControl.js
@@ -1,21 +1,21 @@
-// var block= true;
-// var createProcessPanel;
-// window.addEventListener("load", (e)=>{
+var block= true;
+var createProcessPanel;
+window.addEventListener("load", (e)=>{
 
-//     var createProcess= document.getElementById("createProcess");
-//     createProcessPanel= document.getElementsByClassName("create-process-panel-container")[0];
-//     createProcess.addEventListener("click", (e)=>{
-//         displayer();       
-//     });      
+    var createProcess= document.getElementById("createJob");
+    createProcessPanel= document.getElementsByClassName("create-process-panel-container")[0];
+    createProcess.addEventListener("click", (e)=>{
+        displayer();       
+    });      
 
-// });
+});
 
-// function displayer(){
-//     if(block== true){
-//         createProcessPanel.style.display = "inline-block";
-//         block= false;
-//     }else{
-//         createProcessPanel.style.display = "none";
-//         block= true;
-//     }
-// }
+function displayer(){
+    if(block== true){
+        createProcessPanel.style.display = "inline-block";
+        block= false;
+    }else{
+        createProcessPanel.style.display = "none";
+        block= true;
+    }
+}
