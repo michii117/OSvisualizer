21fda9e43eaa32fa995b344ed3b8841d5eed34a1
Demitri Grant <demitri.grant@gmail.com>
2021-11-27 11:33:55 -0500

    Test Code

--- app/consoleSimulation.js
+++ app/consoleSimulation.js
@@ -1,10 +1,10 @@
 var consoleHover = false;
 
 console.log = function(message){ 
-    
+
     var print =  document.createElement("p") 
-    print.innerHTML = input; 
-    consoleMin.appendChild(message); 
+    print.innerHTML = message; 
+    consoleMin.appendChild(print); 
 
     if(consoleHover == false){
         consoleMin.scrollBy(0,100);
